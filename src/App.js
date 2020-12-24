c22fcb8a1a5d69f2a707d29f4ade548647d8c650
laurencun <lauren.busbee@gmail.com>
2020-12-24 09:51:44 -0600

    fixed import error in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import PostContainer from './components/PostContainer';
 import Profile from './components/Profile';
 import Saved from './components/Saved';
 import { connect } from 'react-redux'
-import '../App.css'
+import './App.css'
 
 
 class App extends Component {
