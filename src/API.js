01f6ca2277ce2e88de6cd39fb0825b1f660f5f3d
laurencun <lauren.busbee@gmail.com>
2021-02-22 20:49:45 -0600

    changed to httpS prod url

--- src/API.js
+++ src/API.js
@@ -1,4 +1,4 @@
-const PROD_URL = 'http://shielded-harbor-32849.herokuapp.com'
+const PROD_URL = 'https://shielded-harbor-32849.herokuapp.com'
 const DEV_URL = 'http://localhost:3000'
 
 const API = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
