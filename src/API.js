45a8edc4042839885524dd8f1541e5bebf1cccc3
laurencun <lauren.busbee@gmail.com>
2021-02-22 20:59:27 -0600

    changed to limitless earth prod url and testing to dev url first

--- src/API.js
+++ src/API.js
@@ -1,6 +1,7 @@
-const PROD_URL = 'https://shielded-harbor-32849.herokuapp.com'
+const PROD_URL = 'http://limitless-earth-02935.herokuapp.com'
 const DEV_URL = 'http://localhost:3000'
 
-const API = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
+const API = DEV_URL
+// const API = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
 
 export default API;
